944d5c6c4c15bea6a119b4af662490410e71d230
Thomas <thomas@enhance.com>
2022-10-17 23:37:52 +0100

    tidying up App.test file

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -45,24 +45,24 @@ test("Starts a new game with the new grid size", async () => {
   expect(screen.getByTestId("x-9-y-9")).toBeInTheDocument();
 });
 
-export const mockStoreNearlyWon = configureStore({
-  reducer: {
-    gameLogicReducer,
-  },
-  preloadedState: {
-    gameLogicReducer: {
-      ...initialState,
-      gridState: [
-        [1024, 1024, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-      ],
-    },
-  },
-});
-
 test("Alerts the player when the game is WON", async () => {
+  const mockStoreNearlyWon = configureStore({
+    reducer: {
+      gameLogicReducer,
+    },
+    preloadedState: {
+      gameLogicReducer: {
+        ...initialState,
+        gridState: [
+          [1024, 1024, 0, 0],
+          [0, 0, 0, 0],
+          [0, 0, 0, 0],
+          [0, 0, 0, 0],
+        ],
+      },
+    },
+  });
+
   render(
     <Provider store={mockStoreNearlyWon}>
       <App />
@@ -77,23 +77,23 @@ test("Alerts the player when the game is WON", async () => {
   expect(global.alert).toHaveBeenCalledWith("You won!");
 });
 
-export const mockStoreNearlyLost = configureStore({
-  reducer: {
-    gameLogicReducer,
-  },
-  preloadedState: {
-    gameLogicReducer: {
-      ...initialState,
-      gridState: [
-        [2, 4, 8],
-        [0, 32, 16],
-        [256, 128, 64],
-      ],
-    },
-  },
-});
-
 test("Alerts the player when the game is LOST", async () => {
+  const mockStoreNearlyLost = configureStore({
+    reducer: {
+      gameLogicReducer,
+    },
+    preloadedState: {
+      gameLogicReducer: {
+        ...initialState,
+        gridState: [
+          [2, 4, 8],
+          [0, 32, 16],
+          [256, 128, 64],
+        ],
+      },
+    },
+  });
+
   render(
     <Provider store={mockStoreNearlyLost}>
       <App />
